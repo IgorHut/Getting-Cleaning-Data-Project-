842acc2141f1c698e93dc8a0dc7f8392eb419791
IgorHut <igorhut@gmail.com>
2015-07-26 20:49:49 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -11,7 +11,7 @@ library('plyr')
 
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileUrl,destfile="./data/UCI_HAR_Dataset.zip",mode='wb') # mode shuld be set to "wb" as .zip is a binary format, if you use mac or work on linux system add method="curl" 
+download.file(fileUrl,destfile="./data/UCI_HAR_Dataset.zip",mode='wb') # mode should be set to "wb" as .zip is a binary format, if you use mac or work on linux system add method="curl" 
 
 # The dataset file can be unziped manualy or as follows
 unzip(zipfile="./data/UCI_HAR_Dataset.zip",exdir="./data")
